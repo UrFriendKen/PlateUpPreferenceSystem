36085c369d2fb50a22a43a6572da72247cb968e6
UrFriendKen <kenneth.chin.work@gmail.com>
2024-08-10 16:03:00 +0800

    Added EffectHelpers patch for PlateUp v1.2.0 issue

---
+++ Patches/ComponentHelpers_Patch.cs
@@ -0,0 +1,40 @@
+﻿using HarmonyLib;
+using Kitchen;
+using KitchenData;
+using KitchenMods;
+using Unity.Entities;
+
+namespace PreferenceSystem.Patches
+{
+    [HarmonyPatch]
+    static class EffectHelpers_Patch
+    {
+        [HarmonyPatch(typeof(EffectHelpers), nameof(EffectHelpers.AddApplianceEffectComponents))]
+        [HarmonyPrefix]
+        [HarmonyPriority(int.MinValue)]
+        static bool AddApplianceEffectComponents_Prefix(bool __runOriginal, EntityCommandBuffer ecb, Entity e, IEffectPropertySource prop)
+        {
+            if (!__runOriginal ||
+                prop.EffectRange == null ||
+                prop.EffectType == null)
+                return true;
+
+            if (!(prop.EffectCondition is IModComponent) &&
+                !(prop.EffectRange is IModComponent) &&
+                !(prop.EffectType is IModComponent))
+                return true;
+
+            ecb.AddComponent(e, default(CAppliesEffect));
+
+            if (prop.EffectCondition == null)
+                ecb.AddComponent(e, default(CEffectAlways));
+            else
+                ecb.AddComponent(e, (dynamic)prop.EffectCondition);
+
+            ecb.AddComponent(e, (dynamic)prop.EffectRange);
+            ecb.AddComponent(e, (dynamic)prop.EffectType);
+
+            return false;
+        }
+    }
+}
